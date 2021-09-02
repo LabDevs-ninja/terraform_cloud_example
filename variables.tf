018ec571a56e497e9e590d6eb2560e66c23a0562
José Menezes <jcarlos78@gmail.com>
2021-09-02 15:00:41 -0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+# Input variable definitions
+
+variable "aws_region" {
+  description = "AWS region for all resources."
+
+  type    = string
+  default = "us-east-1"
+}
+
+variable "master_username" {
+  type    = string
+  default = ""
+}
+
+
+variable "master_password" {
+  type    = string
+  default = ""
+}
