6eb3f544b0102f987b22afd14d8dc107b151b0aa
José Menezes <jcarlos78@gmail.com>
2021-09-09 19:29:13 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,16 +24,6 @@ resource "aws_instance" "app_server" {
   }
 }
 */
-  
-resource "aws_docdb_cluster" "docdb" {
-  cluster_identifier      = "my-docdb-cluster"
-  engine                  = "docdb"
-  master_username         = var.master_username
-  master_password         = var.master_password
-  backup_retention_period = 5
-  preferred_backup_window = "07:00-09:00"
-  skip_final_snapshot     = true
-}
 
 resource "aws_docdb_cluster_instance" "cluster_instances" {
   count              = 1
@@ -47,6 +37,9 @@ resource "aws_docdb_cluster" "default" {
   availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
   master_username    = "foo"
   master_password    = "barbut8chars"
+  backup_retention_period = 5
+  deletion_protection = true
+  skip_final_snapshot     = true
 }
 
 /*Version 1.1.0*/
